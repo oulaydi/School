43099cf2f28a7a3289c39a8e39e49137e21f1b77
Kaneki881 <alarrouchi@gmail.com>
2024-03-06 22:31:06 +0100

    handling hashed password

--- src/models/AddTeacherSchema.js
+++ src/models/AddTeacherSchema.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -52,5 +53,9 @@ const AddTeacherSchema = new Schema({
     },
     createdAt: { type: Date, default: Date.now },
 });
-
+AddTeacherSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 module.exports = mongoose.model("AddTeacher", AddTeacherSchema);
