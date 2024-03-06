d91b49032ae23881fde09a7055847e07867cf981
“oussama <oussama.oulaydi@gmail.com>
2024-03-06 22:36:08 +0100

    comment the old hash line hh

--- src/models/AddTeacherSchema.js
+++ src/models/AddTeacherSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -51,6 +52,12 @@ const AddTeacherSchema = new Schema({
         required: true,
     },
     createdAt: { type: Date, default: Date.now },
+});
+
+AddTeacherSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 module.exports = mongoose.model("AddTeacher", AddTeacherSchema);
